3141e15feade45dc9543ca78c662343eda9f66b6
MaryMoroz <morozova.mary@gmail.com>
2022-12-19 17:39:27 -0500

    add new test

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('My First Test', () => {
+
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io/')
+  })
+
+  it('clicking "type" navigates to a new url', () => {
+    cy.contains('next').click()
+    cy.url().should('include', '/commands/traversal')
+  })
+
+  it('clicking "root" navigates to a new url', () => {
+    cy.contains('root').click()
+  })
+})
